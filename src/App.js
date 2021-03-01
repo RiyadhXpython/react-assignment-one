6db9c3e710ae47d09cae76a89209adc4954f1c50
md riad mahamud sirazy <riadmahamud8@gmail.comm>
2021-03-01 20:21:05 +0800

    finalPush

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Header from './Components/PlayerInfo/Header/Header';
+import Player from './Components/PlayerInfo/Player';
+import Total from './Components/PlayerInfo/TotalBudget/Total'
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [playerCart, setPlayerCart] = useState([]);
+
+
+
+  useEffect(() => {
+    fetch("https://api.mocki.io/v1/344f7018")
+    .then((response) =>response.json())
+    .then(data => setPlayers(data));
+  },[])
+
+
+  const handleAddPlayer = (player) => {
+    const newCart = [...playerCart, player];
+    setPlayerCart(newCart);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  <Header></Header>
+
+  <h1>Total Player : {players.length}</h1>
+  <h2> Player Added : {playerCart.length}</h2>
+  <Total total={playerCart}></Total>
+  
+  
+  <ul>
+    {
+        players.map(player => <Player player={player} handleAddPlayer={handleAddPlayer} key={player.suite}></Player>)
+    }
+  </ul>
+        <header className="App-header">
+          
+        </header>
     </div>
   );
 }
